a4637e46bd104c2bad47b9035702f6ca3a1676a9
yangwan-cw <a2572213252@gmail.com>
2025-09-27 15:37:53 +0800

    feat: popup引入外部样式

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
-console.log("hello world")
+
 
   // 添加事件监听器，点击按钮时打开 options 页面
-  document.getElementById('openOptionsButton').addEventListener('click', function() {
+document.getElementById('openOptionsButton').addEventListener('click', function() {
     console.log("打开配置");
     chrome.runtime.openOptionsPage();
 });
